08e56d41960781e08af5befa78b3a3fb4f83618a
Heroboycloud Akindel <shakiratakindel@gmail.com>
2023-04-22 20:31:39 +0100

    django auth

---
+++ src/my_proj/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include("accounts.urls")),
+    url(r'^$', login_required(TemplateView.as_view(template_name="home.html"))),
+]
